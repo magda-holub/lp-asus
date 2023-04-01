b499861cca14773ab480ddb957c1dfffeaa2180b
magdalena nozarova <magdalena.nozarova@alza.cz>
2023-04-01 16:24:47 +0200

    updated img backup to webp

--- js/script.js
+++ js/script.js
@@ -2,20 +2,19 @@ const layoutSwitch = document.querySelector("#layout_switch");
 const testLayout = document.querySelector("#testLayout");
 const body = document.body;
 
-layoutSwitch.addEventListener("click", function(event) {
-  const button = event.target;
-  
-  if (button.matches("button:first-child")) {
-    testLayout.classList.remove("layout-0", "layout-1");
-    body.classList.add("mobile");
-    console.log("mobile layout");
-  } else if (button.matches("button:nth-child(2)")) {
-    testLayout.classList.add("layout-0");
-    body.classList.remove("mobile");
-    console.log("3 column layout");
-  } else if (button.matches("button:nth-child(3)")) {
-    testLayout.classList.add("layout-1");
-    body.classList.remove("mobile");
-    console.log("4 column layout");
-  }
+layoutSwitch.querySelectorAll("button")[0].addEventListener("click", function () {
+  testLayout.classList.remove("layout-0", "layout-1");
+  body.classList.add("mobile");
+});
+
+layoutSwitch.querySelectorAll("button")[1].addEventListener("click", function () {
+  testLayout.classList.remove("layout-1");
+  body.classList.remove("mobile");
+  testLayout.classList.add("layout-0");
+});
+
+layoutSwitch.querySelectorAll("button")[2].addEventListener("click", function () {
+  testLayout.classList.remove("layout-0");
+  body.classList.remove("mobile");
+  testLayout.classList.add("layout-1");
 });
