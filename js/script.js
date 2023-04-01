d0706dfaeb770c1cb420062685eefd8c7fca135e
magdalena nozarova <magdalena.nozarova@alza.cz>
2023-04-01 14:45:43 +0200

    updated switcher

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-$("#layout_switch button").on("click",function(){$("#testLayout").removeClass("layout-0").removeClass("layout-1"),$("body").addClass("mobile")}),$("#layout_switch button:nth-child(2)").on("click",function(){$("#testLayout").addClass("layout-0"),$("body.mobile").removeClass("mobile")}),$("#layout_switch button:nth-child(3)").on("click",function(){$("#testLayout").addClass("layout-1"),$("body.mobile").removeClass("mobile")});
+const layoutSwitch = document.querySelector("#layout_switch");
+const testLayout = document.querySelector("#testLayout");
+const body = document.body;
+
+layoutSwitch.addEventListener("click", function(event) {
+  const button = event.target;
+  
+  if (button.matches("button:first-child")) {
+    testLayout.classList.remove("layout-0", "layout-1");
+    body.classList.add("mobile");
+    console.log("mobile layout");
+  } else if (button.matches("button:nth-child(2)")) {
+    testLayout.classList.add("layout-0");
+    body.classList.remove("mobile");
+    console.log("3 column layout");
+  } else if (button.matches("button:nth-child(3)")) {
+    testLayout.classList.add("layout-1");
+    body.classList.remove("mobile");
+    console.log("4 column layout");
+  }
+});
