e0b0454f5cc2639e9c4fc02176f97b38735ecce6
magdalena nozarova <magdalena.nozarova@alza.cz>
2023-04-01 16:29:15 +0200

    switcher.js jquery version

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-const layoutSwitch = document.querySelector("#layout_switch");
+const switcher = document.querySelector("#layout_switch");
 const testLayout = document.querySelector("#testLayout");
 const body = document.body;
 
-layoutSwitch.querySelectorAll("button")[0].addEventListener("click", function () {
+switcher.querySelectorAll("button")[0].addEventListener("click", function () {
   testLayout.classList.remove("layout-0", "layout-1");
   body.classList.add("mobile");
 });
 
-layoutSwitch.querySelectorAll("button")[1].addEventListener("click", function () {
+switcher.querySelectorAll("button")[1].addEventListener("click", function () {
   testLayout.classList.remove("layout-1");
   body.classList.remove("mobile");
   testLayout.classList.add("layout-0");
 });
 
-layoutSwitch.querySelectorAll("button")[2].addEventListener("click", function () {
+switcher.querySelectorAll("button")[2].addEventListener("click", function () {
   testLayout.classList.remove("layout-0");
   body.classList.remove("mobile");
   testLayout.classList.add("layout-1");
