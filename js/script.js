75d2ec1a1744b1a6aa726335a85709bd71ee0e1b
Magdalena Holubova <mholubova@proton.me>
2023-04-08 02:09:25 +0200

    updated switcher transition on buttons

--- js/script.js
+++ js/script.js
@@ -1,20 +1,34 @@
 const switcher = document.querySelector("#layout_switch");
+const buttons = switcher.querySelectorAll("button")
 const testLayout = document.querySelector("#testLayout");
 const body = document.body;
 
 switcher.querySelectorAll("button")[0].addEventListener("click", function () {
   testLayout.classList.remove("layout-0", "layout-1");
   body.classList.add("mobile");
+  buttons.forEach(function(button) {
+    button.classList.remove("active")
+  });
+  this.classList.add("active");
+
 });
 
 switcher.querySelectorAll("button")[1].addEventListener("click", function () {
   testLayout.classList.remove("layout-1");
   body.classList.remove("mobile");
   testLayout.classList.add("layout-0");
+  buttons.forEach(function(button) {
+    button.classList.remove("active")
+  });
+  this.classList.add("active");
 });
 
 switcher.querySelectorAll("button")[2].addEventListener("click", function () {
   testLayout.classList.remove("layout-0");
   body.classList.remove("mobile");
   testLayout.classList.add("layout-1");
+  buttons.forEach(function(button) {
+    button.classList.remove("active")
+  });
+  this.classList.add("active");
 });
